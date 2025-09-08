430e264bd64d1d937e372a80022d1d800f57d0ee
lucas (lab novo) <lucasccesar05@gmail.com>
2025-09-08 10:01:52 -0300

    added sum function

---
+++ Operations.java
@@ -0,0 +1,11 @@
+public class Operations {
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static double sum(double a, double b) {
+        return a + b;
+    }
+
+}
